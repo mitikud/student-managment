a01fdb0ac6a714663a9199a82940652ce6db17dd
mitikud <mitikud@gmail.com>
2025-03-23 22:16:36 -0400

    auth and student services

---
+++ aut-service/src/main/java/com/studentmanagment/aut_service/AutServiceApplication.java
@@ -0,0 +1,13 @@
+package com.studentmanagment.aut_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutServiceApplication.class, args);
+	}
+
+}
---
+++ aut-service/src/main/java/com/studentmanagment/aut_service/entity/UserModel.java
@@ -0,0 +1,32 @@
+package com.studentmanagment.aut_service.entity;
+
+//import jakarta.persistence.*;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+
+@Table(name = "users", schema = "auth")
+public class UserModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Username cannot be blank")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+    private String username;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+
+    private String role;
+}
